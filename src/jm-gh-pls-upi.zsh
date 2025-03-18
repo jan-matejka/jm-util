09a347d65362caca36ac6d382ec758c8f1ad0312
Jan Matějka <jan@matejka.ninja>
2025-03-18 12:18:10 +0100

    fix(src/jm-gh-pls-upi.zsh): reorder fields and add id

--- src/jm-gh-pls-upi.zsh
+++ src/jm-gh-pls-upi.zsh
@@ -4,15 +4,16 @@
 SELF="${0##*/}"
 . jm_prelude
 
-fmt="%3v %v %11v %10v %5v %v"
+fmt="%3v %v %10v %5v %v %11v %v"
 
-header="{{ tablerow (printf "$fmt" "id" "type" "status" "it.start" "it.duration" "title" ) }}\\"
+#                                  1     2      3          4             5    6
+header="{{ tablerow (printf "$fmt" "num" "type" "it.start" "it.duration" "id" "status" "title" ) }}\\"
 header="" # override
 
 tpl=$(cat << EOF
 $header\
 {{ range .items }}\
-{{ tablerow (printf "$fmt" .content.number .content.type .status .iteration.startDate .iteration.duration .title) }}\
+{{ tablerow (printf "$fmt" .content.number .content.type .iteration.startDate .iteration.duration .id .status .title) }}\
 {{ end }}
 EOF
 )
@@ -22,7 +23,7 @@ jq=$(cat << EOF
 EOF
 )
 
-current=2025-03
+current=$(date +%Y-%m)
 
 gh_args=( \
   --owner @me
@@ -34,12 +35,12 @@ gh_args=( \
   $@
 )
 
+# Note: fields after .status not addressable because status contains whitespace
 awk_cond=(
   '$2 == "Issue"'
-  '&& $3 != "Done"'
-  '&& $4 !~ "'$current'"'
-  '&& $4 != "<nil>"'
+  '&& $6 != "Done"'
+  '&& $3 !~ "'$current'"'
+  '&& $3 != "<nil>"'
 )
 
-gh project item-list $gh_args | \
-  awk "$awk_cond { print }"
+gh project item-list $gh_args | awk "$awk_cond { print }"
