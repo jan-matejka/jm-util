f38cb1b4d907cf883d29b8fe36a5471043fe773c
Jan Matějka <jan@matejka.ninja>
2025-07-10 21:34:46 +0200

    fix(jm-lcpp): fix not considering all paths; fixes #10

--- src/jm-lcpp.rs
+++ src/jm-lcpp.rs
@@ -1,66 +1,42 @@
-use std::io;
-use std::path::Path;
-use std::path::Component;
-use std::path::PathBuf;
+use std::io::{stdin,Lines,StdinLock};
 
-fn lcpp<'x>(x: Vec<Component<'x>>, y: Vec<Component>) -> Vec<Component<'x>> {
-    let lcpp:Vec<_> = x.iter()
-            .zip(y.iter())
-            .take_while(|(x, y)| x==y)
-            .map(|(x, _) | *x)
-            .collect();
-    return lcpp.clone();
+fn lcp(mut lines: Lines<StdinLock<'_>>) -> Option<String> {
+    // longest common prefix
+    let prefix_o = lines.next();
+    if prefix_o.is_none()
+        { return None; }
+
+    let prefix_rs = prefix_o.unwrap();
+    let mut prefix = prefix_rs.unwrap();
+
+    for ln in lines {
+        let lns = ln.unwrap();
+        while !lns.starts_with(&prefix) {
+            prefix.pop();
+            if prefix.is_empty()
+                { return None; }
+        }
+    }
+    return Some(prefix);
+}
+
+fn drop_trailing_basename(s: &mut String) {
+    if !s.contains("/")
+        { s.truncate(0); return; }
+
+    while !s.ends_with("/")
+        { s.pop(); }
+    s.pop();
 }
 
 fn main() {
-    let stdin = io::stdin();
-    let lines: Vec<String> = stdin.lines().map(|x| x.unwrap()).collect();
-
-    if lines.len() == 0 {
-        return;
-    }else if lines.len() == 1 {
-        println!("{}", lines[0]);
+    let lcp = lcp(stdin().lines());
+    if lcp.is_none() {
+        // println!("none");
         return;
     }
-
-    let mut paths: Vec<Vec<Component>> = (0..lines.len())
-        .map(|i| Path::new(&lines[i]).components().collect())
-        .collect();
-
-    let lens: Vec<usize> = paths.iter().map(|x| x.len()).collect();
-    let min:usize = *lens.iter().min().unwrap();
-    let max:usize = *lens.iter().max().unwrap();
-
-    let mut compare: Vec<Vec<Component>>;
-    if min == max {
-        compare = paths;
-    }else{
-        // Note we need to select and compare all the minimal and maximal elements here.
-        // (Instead of picking one at random as they are not interchangeable as is the case in the
-        // longest common string prefix problem).
-        compare = Vec::new();
-        let mut i = 0;
-        while i < paths.len() {
-            let minmax = vec![min, max];
-            if minmax.contains(&paths[i].len()) {
-                compare.push(paths.remove(i));
-            }else{
-                i+=1;
-            }
-        }
-    }
-
-    while compare.len() > 1 {
-        let x = compare.pop().unwrap();
-        let y = compare.pop().unwrap();
-        let new = lcpp(x, y);
-        compare.push(new);
-    }
-
-    let components = compare.pop().unwrap();
-    let rs = components.iter().fold(PathBuf::new(), |x, y| x.join(y));
-    let r = rs.to_str().unwrap();
-    if r != "" {
-        println!("{}", r);
-    }
+    let mut lcp = lcp.unwrap();
+    drop_trailing_basename(&mut lcp);
+    if !lcp.is_empty()
+        { println!("{}", lcp); }
 }
