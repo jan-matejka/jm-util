55a4f548f8a8f0fbe57783fad8a45f5d05d302c4
Jan Matějka <jan@matejka.ninja>
2026-08-23 06:58:12 +0200

    fx(completion): add missing commands and arguments

--- completion/jm.bash
+++ completion/jm.bash
@@ -2,7 +2,22 @@
 
 [[ -n ${ZSH_VERSION} ]] && autoload bashcompinit
 
-_jm_cmds=( backlight certinfo keymap offending xlock )
+_jm_cmds=(
+  backlight
+  certinfo
+  claude
+  container-env-setup
+  exec
+  find-git
+  gh
+  keymap
+  lcpp
+  offending
+  tmux-autosession
+  version
+  xlock
+)
+
 function _jm_completion {
   case $COMP_CWORD in
   1)
@@ -11,6 +26,19 @@ function _jm_completion {
   2)
     if [[ ${COMP_WORDS[1]} == "keymap" ]]; then
       COMPREPLY=($(compgen -W "set toggle" ${COMP_WORDS[2]}))
+    elif [[ ${COMP_WORDS[1]} == "backlight" ]]; then
+      COMPREPLY=($(compgen -W "--max" ${COMP_WORDS[2]}))
+    elif [[ ${COMP_WORDS[1]} == "certinfo" ]]; then
+      COMPREPLY=($(compgen -W "-h -p" ${COMP_WORDS[2]}))
+    elif [[ ${COMP_WORDS[1]} == "claude" ]]; then
+      COMPREPLY=($(compgen -W "-p --primary" ${COMP_WORDS[2]}))
+    elif [[ ${COMP_WORDS[1]} == "gh" ]]; then
+      COMPREPLY=($(compgen -W "pls-upi move-upi" ${COMP_WORDS[2]}))
+    fi
+    ;;
+  3)
+    if [[ ${COMP_WORDS[2]} == "move-upi" ]]; then
+      COMPREPLY=($(compgen -W "-n" ${COMP_WORDS[2]}))
     fi
     ;;
   *)
