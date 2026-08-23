cfd38beccb1ddde9e34cfed64093bbe51a4d73af
Jan Matějka <jan@matejka.ninja>
2026-08-23 10:18:10 +0200

    fx(completion): add separator

--- completion/jm.bash
+++ completion/jm.bash
@@ -21,24 +21,24 @@ _jm_cmds=(
 function _jm_completion {
   case $COMP_CWORD in
   1)
-    COMPREPLY=($(compgen -W "${_jm_cmds[*]}" "${COMP_WORDS[1]}"))
+    COMPREPLY=($(compgen -W "${_jm_cmds[*]}" -- "${COMP_WORDS[1]}"))
     ;;
   2)
     if [[ ${COMP_WORDS[1]} == "keymap" ]]; then
-      COMPREPLY=($(compgen -W "set toggle" ${COMP_WORDS[2]}))
+      COMPREPLY=($(compgen -W "set toggle" -- "${COMP_WORDS[2]}"))
     elif [[ ${COMP_WORDS[1]} == "backlight" ]]; then
-      COMPREPLY=($(compgen -W "--max" ${COMP_WORDS[2]}))
+      COMPREPLY=($(compgen -W "--max" -- "${COMP_WORDS[2]}"))
     elif [[ ${COMP_WORDS[1]} == "certinfo" ]]; then
-      COMPREPLY=($(compgen -W "-h -p" ${COMP_WORDS[2]}))
+      COMPREPLY=($(compgen -W "-h -p" -- "${COMP_WORDS[2]}"))
     elif [[ ${COMP_WORDS[1]} == "claude" ]]; then
-      COMPREPLY=($(compgen -W "-p --primary" ${COMP_WORDS[2]}))
+      COMPREPLY=($(compgen -W "-p --primary" -- "${COMP_WORDS[2]}"))
     elif [[ ${COMP_WORDS[1]} == "gh" ]]; then
-      COMPREPLY=($(compgen -W "pls-upi move-upi" ${COMP_WORDS[2]}))
+      COMPREPLY=($(compgen -W "pls-upi move-upi" -- "${COMP_WORDS[2]}"))
     fi
     ;;
   3)
     if [[ ${COMP_WORDS[2]} == "move-upi" ]]; then
-      COMPREPLY=($(compgen -W "-n" ${COMP_WORDS[2]}))
+      COMPREPLY=($(compgen -W "-n" -- "${COMP_WORDS[2]}"))
     fi
     ;;
   *)
