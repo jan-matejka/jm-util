43c58aacf49f33de40979f52cbe728db5e4357bf
Jan Matějka <jan@matejka.ninja>
2026-09-04 12:46:03 +0200

    rf:git-cif: single file automation goes before cc scope construction

--- git-cif/git-cif.zsh
+++ git-cif/git-cif.zsh
@@ -98,11 +98,8 @@ $o_all && {
     lcpp=$(print -r -- "$lcpp" | sed "${sed_args[@]}")
   }
 
-  # add scope
-  [[ -n $lcpp ]] && cc+=( "$lcpp" )
-
   if (( ${#st_xy} == 1 )); then
-    # Single file commit. Add change type markers.
+    # Single file commit automation.
     case ${st_xy[1]:0:1} in
     A)
       o_msg="add ${o_msg}"
@@ -112,6 +109,9 @@ $o_all && {
       ;;
     esac
   fi
+
+  # add scope
+  [[ -n $lcpp ]] && cc+=( "$lcpp" )
 
   subject=${(j.:.)cc}
 
