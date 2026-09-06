b0dd27e6b0c6c8868e47804bbc0dae664d91be07
Jan Matějka <jan@matejka.ninja>
2026-09-06 11:39:06 +0200

    ft:git-suite: default rev-range to main..

--- git-suite/git-l.zsh
+++ git-suite/git-l.zsh
@@ -12,4 +12,21 @@ args=( --oneline --decorate --graph )
 has_opt -S && args+=( -S ${paargs[-S]} --no-graph )
 has_opt -G && args+=( -G ${paargs[-G]} --no-graph )
 
-exec git log $args
+# cases:
+# --grep <s>
+# --grep <s> <rev-range>
+# --grep <s> -- <path>
+# -i <rev-range>
+# general:
+# --opt <arg>
+# FIXME: There is no way to parse out rev-range while letting opts passthrough
+# generically bc we don't know if <arg> is opt-val or narg.
+if ! (( $# )); then
+  def=$(git config get --default main init.defaultBranch )
+  cur=$(git branch --show-current)
+  if [[ $def != $cur ]]; then
+    args+=( "${def}.." )
+  fi
+fi
+
+exec git log $args $@
