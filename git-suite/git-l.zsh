6e28e8daf2dfc6f43dfb8af41af2ae4e10292432
Jan Matějka <jan@matejka.ninja>
2026-09-06 12:02:59 +0200

    ft:git-suite/git-l: no --graph for -G

--- git-suite/git-l.zsh
+++ git-suite/git-l.zsh
@@ -5,10 +5,11 @@ SELF="${0##*/}"
 set -e
 
 declare -A paargs
-zparseopts -K -D -E -A paargs S:
+zparseopts -K -D -E -A paargs S: G:
 
 args=( --oneline --decorate --graph )
 
 has_opt -S && args+=( -S ${paargs[-S]} --no-graph )
+has_opt -G && args+=( -G ${paargs[-G]} --no-graph )
 
 exec git log $args
