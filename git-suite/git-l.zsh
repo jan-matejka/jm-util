e97ed1c2b201ec198a7ea7e8d19b08001e7c0463
Jan Matějka <jan@matejka.ninja>
2026-09-06 11:41:19 +0200

    ft:git-suite/git-l: no --graph if -S

--- git-suite/git-l.zsh
+++ git-suite/git-l.zsh
@@ -4,5 +4,11 @@ SELF="${0##*/}"
 . jm_prelude
 set -e
 
-args=( --oneline --decorate --graph)
+declare -A paargs
+zparseopts -K -D -E -A paargs S:
+
+args=( --oneline --decorate --graph )
+
+has_opt -S && args+=( -S ${paargs[-S]} --no-graph )
+
 exec git log $args
